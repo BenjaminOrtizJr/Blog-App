723ee29272cf996969d114fcbeb52f8d09c5c9f0
BenjaminOrtizJr <bortizjr84@outlook.com>
2025-10-20 21:21:14 -0400

    #

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -4,6 +4,11 @@ const Header = () => {
   return (
     <header>
       <h1>My Blog</h1>
+      <nav>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+      </nav>
     </header>
   );
 };
