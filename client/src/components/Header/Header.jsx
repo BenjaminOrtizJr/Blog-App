16f3f11191cb122940108341450aa47eaec0cc53
BenjaminOrtizJr <bortizjr84@outlook.com>
2025-10-21 23:12:24 -0400

    #

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import { MdLightMode } from "react-icons/md";
+import { BsToggle2Off } from "react-icons/bs";
 
 const Header = () => {
   return (
     <header>
-      <h1>My Blog</h1>
-      <nav>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/contact">Contact</a>
+      <h2>My Blog</h2>
+      <nav aria-label="Main navigation">
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
       </nav>
+      <MdLightMode />
+      <BsToggle2Off />
     </header>
   );
 };
